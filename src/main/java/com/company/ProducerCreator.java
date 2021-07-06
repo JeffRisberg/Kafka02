35a229998023c7342a1219c145b8bd04a3869c47
JeffRisberg <j.s.risberg@gmail.com>
2021-07-05 23:08:20 -0700

    add byte array seralize/deseralize

--- src/main/java/com/company/ProducerCreator.java
+++ src/main/java/com/company/ProducerCreator.java
@@ -1,26 +1,27 @@
 package com.company;
 
-import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.LongSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 /**
  * @author Jeff Risberg
  * @since 08/01/18
  */
 public class ProducerCreator {
-    public static Producer<Long, String> createProducer() {
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, IKafkaConstants.GROUP_ID_CONFIG);
+  public static Producer<Long, byte[]> createProducer() {
+    Properties props = new Properties();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, IKafkaConstants.GROUP_ID_CONFIG);
 
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
 
-        return new KafkaProducer<>(props);
-    }
+    return new KafkaProducer<>(props);
+  }
 }
