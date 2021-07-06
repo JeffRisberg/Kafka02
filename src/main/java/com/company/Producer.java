52a3f02a929158b32ac9442a54763dd43e6a4912
JeffRisberg <j.s.risberg@gmail.com>
2021-07-05 19:10:44 -0700

    merged code structure from Kafka01

--- src/main/java/com/company/Producer.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-/**
- * @author Jeff Risberg
- * @since 08/01/18
- */
-public class Producer {
-
-}
---
+++ src/main/java/com/company/ProducerCreator.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+/**
+ * @author Jeff Risberg
+ * @since 08/01/18
+ */
+public class ProducerCreator {
+    public static Producer<Long, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, IKafkaConstants.GROUP_ID_CONFIG);
+
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return new KafkaProducer<>(props);
+    }
+}
