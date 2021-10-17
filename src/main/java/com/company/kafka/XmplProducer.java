86d5e02c7f27667a6c1b02b32955536e3e31da2d
JeffRisberg <j.s.risberg@gmail.com>
2021-10-16 20:29:11 -0700

    handle generated files, begin to use schema registry

--- src/main/java/com/company/kafka/XmplProducer.java
+++ src/main/java/com/company/kafka/XmplProducer.java
@@ -2,23 +2,38 @@ package com.company.kafka;
 
 import static com.company.kafka.ProducerCreator.createProducer;
 
+import com.company.kafka.constants.IKafkaConstants;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 public class XmplProducer {
 
   public static void main(String[] args) throws Exception {
 
-    com.kakfainaction.Alert x = new com.kakfainaction.Alert();
-
     String topicName = "users.registrations";
 
-    Producer<Long, String> producer = createProducer();
+    Properties props = new Properties();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
+
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+            LongSerializer.class.getName());
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+            io.confluent.kafka.serializers.KafkaAvroSerializer.class.getName());
+    props.put("schema.registry.url", "http://localhost:8081");
+
+    Producer<Long, com.kakfainaction.Alert> producer = new KafkaProducer<>(props);
 
     for (int i = 0; i < 10; i++) {
-      String message = "This is record " + i;
+      com.kakfainaction.Alert alert = new com.kakfainaction.Alert();
+      alert.setStatus("Pending");
 
-      producer.send(new ProducerRecord<>(topicName, new Long(i), message));
+      producer.send(new ProducerRecord<>(topicName, new Long(i), alert));
 
       System.out.println("Message sent successfully");
     }
